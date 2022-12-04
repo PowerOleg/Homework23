5ebe265dc4e6132b7b94e93237308651639866b2
PowerOleg <geraskin.oleg@mail.ru>
2022-12-04 16:59:29 +0500

    done but need to clean

--- src/Basket2.java
+++ src/Basket2.java
@@ -11,34 +11,7 @@ public class Basket2 {
         this.prices = prices;
     }
 
-    public void addToCart(int productNum, int number) {
-        this.quantitylist[productNum] += number;
-    }
-    public void printCart() {
-        System.out.println("The product list for purchase:");
-        for (int i = 0; i < goodsList.length; i++) {
-            System.out.printf("%d. %s %d rub/piece\n", i + 1, goodsList[i], prices[i]);
-        }
-    }
-    public void saveTxt(File textFile) {
-        try (BufferedWriter out = new BufferedWriter(new PrintWriter(textFile))) {
-            for (String e : goodsList)
-                out.append(e + " ");
-                out.newLine();
-            for (int i : prices)
-                out.append(i + " ");
-                out.newLine();
-            for (int i : quantitylist)
-                out.append(i + " ");
-                out.newLine();
-                out.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-//надо попробовать вынести объявление BufferedReader из метода
+    //надо попробовать вынести объявление BufferedReader из метода
     public static Basket2 loadFromTxtFile(File textFile) {
         String[] goodsList;
         int[] quantitylist;
@@ -67,28 +40,79 @@ public class Basket2 {
         return basket2;
     }
 
+    public void addToCart(int productNum, int number) {
+        this.quantitylist[productNum] += number;
+    }
+
+    public void printCart() {
+        System.out.println("The product list for purchase:");
+        for (int i = 0; i < goodsList.length; i++) {
+            System.out.printf("%d. %s %d rub/piece\n", i + 1, goodsList[i], prices[i]);
+        }
+    }
+
+    public void saveTxt(File textFile) {
+        try (BufferedWriter out = new BufferedWriter(new PrintWriter(textFile))) {
+            for (String e : goodsList)
+                out.append(e + " ");
+            out.newLine();
+            for (int i : prices)
+                out.append(i + " ");
+            out.newLine();
+            for (int i : quantitylist)
+                out.append(i + " ");
+            out.newLine();
+            out.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void result() {
+        int total = 0;
+        int lineNumber = 1;
+        int productIterationNumber = 0;
+        System.out.println("Orders:");
+        for (int i : this.quantitylist) {
+            if (i != 0) {
+                int priceInOrder = i * this.prices[productIterationNumber];
+                total += priceInOrder;
+                System.out.printf("%d. %s %d pieces %d rub/piece %d rub in order\n",
+                        lineNumber, this.goodsList[productIterationNumber], i,
+                        this.prices[productIterationNumber], priceInOrder);
+                lineNumber++;
+            }
+            productIterationNumber++;
+        }
+        System.out.println("Total: " + total + " rubles");
+    }
+
 
     public String[] getGoodsList() {
         return goodsList;
-    }
-
-    public int[] getQuantitylist() {
-        return quantitylist;
-    }
-
-    public int[] getPrices() {
-        return prices;
     }
 
     public void setGoodsList(String[] goodsList) {
         this.goodsList = goodsList;
     }
 
+    public int[] getQuantitylist() {
+        return quantitylist;
+    }
+
     public void setQuantitylist(int[] quantitylist) {
         this.quantitylist = quantitylist;
+    }
+
+    public int[] getPrices() {
+        return prices;
     }
 
     public void setPrices(int[] prices) {
         this.prices = prices;
     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int total = 0;
-//    static String[] goodsList = {"Bread(500g)", "Milk(1000g)", "Buckwheat(900g)",
-//            "Rice(900g)", "Butter(175g)", "Broiler_Chicken_Thighs(900g)", "Tea(20*1.5g)"};
-//    static int[] quantitylist = new int[goodsList.length];
-//    static int[] prices = {21, 64, 129, 156, 129, 216, 120};
-
-
-
-
-
-
-
-
-
-
-
-
-    //переделать чтобы выводил исходя из того что в basket.txt
-//    public static void result() {
-//        int lineNumber = 1;
-//        int productIterationNumber = 0;
-//        System.out.println("Orders:");
-//        for (int i : quantitylist) {
-//            if (i != 0) {
-//                int priceInOrder = i * prices[productIterationNumber];
-//                total += priceInOrder;
-//                System.out.printf("%d. %s %d pieces %d rub/piece %d rub in order\n",
-//                        lineNumber, goodsList[productIterationNumber], i,
-//                        prices[productIterationNumber], priceInOrder);
-//                lineNumber++;
-//            }
-//            productIterationNumber++;
-//        }
-//        System.out.println("Total: " + total + " rubles");
-//    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         File file = new File("basket.txt");
@@ -58,15 +21,13 @@ public class Main {
         }
 
 
-
-
         basket.printCart();
         while (true) {
             System.out.println("Choose a product, quantity or input end");
             System.out.print(">>");
             String input = scanner.nextLine();
             if (input.toLowerCase().equals("end")) {
-                result();
+                basket.result();
                 break;
             }
             String[] parts = input.split(" ");
@@ -75,20 +36,20 @@ public class Main {
                 continue;
             }
 //!!надо подумать как basket.getGoodsList сделать чтоб он воспринимал разные basket
-                try {
-                    if (Integer.parseInt(parts[0]) > basket.getGoodsList().length || Integer.parseInt(parts[0]) <= 0) {
-                        System.out.println("Please input a correct product number");
-                        continue;
-                    }
-                    if (Integer.parseInt(parts[1]) < 0) {
-                    System.out.println("Goods quantity should be a positive number," +
-                        " please input a correct number");
-                    continue;
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("You entered not a number, please input a number");
+            try {
+                if (Integer.parseInt(parts[0]) > basket.getGoodsList().length || Integer.parseInt(parts[0]) <= 0) {
+                    System.out.println("Please input a correct product number");
                     continue;
                 }
+                if (Integer.parseInt(parts[1]) < 0) {
+                    System.out.println("Goods quantity should be a positive number," +
+                            " please input a correct number");
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("You entered not a number, please input a number");
+                continue;
+            }
             int productNumber = (Integer.parseInt(parts[0]) - 1);
             int quantity = Integer.parseInt(parts[1]);
 
