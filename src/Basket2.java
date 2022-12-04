340854666bbe638fd7fb39680212c07459b9f9bd
PowerOleg <geraskin.oleg@mail.ru>
2022-12-04 15:19:29 +0500

    Basket2 all methods are done

--- src/Basket2.java
+++ src/Basket2.java
@@ -8,14 +8,19 @@ public class Basket2 {
     private int[] quantitylist;
     private int[] prices;
 
-    public Basket2(String[] goodsList, int[] prices, int[] quantitylist) {
+    public Basket2(String[] goodsList, int[] prices) {
         this.goodsList = goodsList;
         this.prices = prices;
-        this.quantitylist = quantitylist;
     }
 
-    public void addToCart(int productNum, int amount) {
-
+    public void addToCart(int productNum, int number) {
+        this.quantitylist[productNum] += number;
+    }
+    public void printCart() {
+        System.out.println("The product list for purchase:");
+        for (int i = 0; i < goodsList.length; i++) {
+            System.out.printf("%d. %s %d rub/piece\n", i + 1, goodsList[i], prices[i]);
+        }
     }
     public void saveTxt(File textFile) {
         try (BufferedWriter out = new BufferedWriter(new PrintWriter(textFile))) {
@@ -59,7 +64,9 @@ public class Basket2 {
         prices = Arrays.stream(o[1].toString().split(" ")).mapToInt(n -> Integer.parseInt(n)).toArray();
         quantitylist = Arrays.stream(o[2].toString().split(" ")).mapToInt(n -> Integer.parseInt(n)).toArray();
 
-        return new Basket2(goodsList, prices, quantitylist);
+        Basket2 basket2 = new Basket2(goodsList, prices);
+        basket2.setQuantitylist(quantitylist);
+        return basket2;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     static int total = 0;
-    static String[] goodsList = {"Bread(500g)", "Milk(1000g)", "Buckwheat(900g)",
-            "Rice(900g)", "Butter(175g)", "Broiler_Chicken_Thighs(900g)", "Tea(20*1.5g)"};
-    static int[] quantitylist = new int[goodsList.length];
-    static int[] prices = {21, 64, 129, 156, 129, 216, 120};
+//    static String[] goodsList = {"Bread(500g)", "Milk(1000g)", "Buckwheat(900g)",
+//            "Rice(900g)", "Butter(175g)", "Broiler_Chicken_Thighs(900g)", "Tea(20*1.5g)"};
+//    static int[] quantitylist = new int[goodsList.length];
+//    static int[] prices = {21, 64, 129, 156, 129, 216, 120};
 
 
 
 
 
 
-    //нужно чтобы метод выводил исходя и записей в basket.txt
-//    public static void printGoods() {
-//        System.out.println("The product list for purchase:");
-//        for (int i = 0; i < goodsList.length; i++) {
-//            System.out.printf("%d. %s %d rub/piece\n", i + 1, goodsList[i], prices[i]);
-//        }
-//    }
+
 
 
 
@@ -46,47 +40,49 @@ public class Main {
 //    }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         File file = new File("basket.txt");
-//        Basket2 basket2 = Basket2.loadFromTxtFile(file);
+        Basket2 basket = Basket2.loadFromTxtFile(file);
 //        System.out.println(Arrays.toString(basket2.getGoodsList()));
 //        System.out.println(Arrays.toString(basket2.getPrices()));
 //        System.out.println(Arrays.toString(basket2.getQuantitylist()));
-        Basket2 basket = new Basket2(goodsList, prices, quantitylist);
-        basket.saveTxt(file);
+
+
+
+        basket.printCart();
+        while (true) {
+            System.out.println("Choose a product, quantity or input end");
+            System.out.print(">>");
+            String input = scanner.nextLine();
+            if (input.toLowerCase().equals("end")) {
+//!!                result();
+                break;
+            }
+            String[] parts = input.split(" ");
+            if (parts.length != 2) {
+                System.out.println("You haven't written two numbers, please try again");
+                continue;
+            }
+            //надо подумать как basket.getGoodsList сделать чтоб он воспринимал разные basket
+                try {
+                    if (Integer.parseInt(parts[0]) > basket.getGoodsList().length || Integer.parseInt(parts[0]) <= 0) {
+                        System.out.println("Please input a correct product number");
+                        continue;
+                    }
+                    if (Integer.parseInt(parts[1]) < 0) {
+                    System.out.println("Goods quantity should be a positive number," +
+                        " please input a correct number");
+                    continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("You entered not a number, please input a number");
+                    continue;
+                }
+            int productNumber = (Integer.parseInt(parts[0]) - 1);
+            int quantity = Integer.parseInt(parts[1]);
+
+            basket.addToCart(productNumber, quantity);
+            basket.saveTxt(file);
+        }
     }
-//        Scanner scanner = new Scanner(System.in);
-//        printGoods();
-//        while (true) {
-//            System.out.println("Choose a product, quantity or input end");
-//            System.out.print(">>");
-//            String input = scanner.nextLine();
-//            if (input.toLowerCase().equals("end")) {
-//                result();
-//                break;
-//            }
-//            String[] parts = input.split(" ");
-//            if (parts.length != 2) {
-//                System.out.println("You haven't written two numbers, please try again");
-//                continue;
-//            }
-//                try {
-//                    if (Integer.parseInt(parts[0]) > goodsList.length || Integer.parseInt(parts[0]) <= 0) {
-//                        System.out.println("Please input a correct product number");
-//                        continue;
-//                    }
-//                    if (Integer.parseInt(parts[1]) < 0) {
-//                    System.out.println("Goods quantity should be a positive number," +
-//                        " please input a correct number");
-//                    continue;
-//                    }
-//                } catch (NumberFormatException e) {
-//                    System.out.println("You entered not a number, please input a number");
-//                    continue;
-//                }
-//            int productNumber = (Integer.parseInt(parts[0]) - 1);
-//            int quantity = Integer.parseInt(parts[1]);
-//
-//            quantitylist[productNumber] += quantity;
-//        }
-//    }
 }
